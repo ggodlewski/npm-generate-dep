08d3e1aa056e4e27d44a8ea5bf86fc5b6a79dbe8
Grzegorz Godlewski <gg@gitgis.com>
2016-12-22 02:13:05 +0100

    Separate dev and deps

--- README.md
+++ README.md
@@ -7,18 +7,35 @@ Generates package.json dependencies from separate files
 package.dev.json
 ```json
 {
-    "npm-run-all": "^3.1.2",
-    "nodemon": "^1.11.0"
-}
-```
+    "dependencies":
+    {
+        "babel-runtime": "^6.20.0"
+    },
+    "devDependencies":
+    {
+        "babel-core": "^6.13.2",
+        "babel-loader": "^6.2.5",
+        "babel-plugin-transform-runtime": "^6.15.0",
+        "babel-polyfill": "^6.13.0",
+        "babel-preset-es2015": "^6.13.2",
+        "babel-preset-stage-0": "^6.16.0",
+        "webpack": "2.1.0-beta.25",
+        "webpack-dev-middleware": "1.8.3",
+        "webpack-dev-server": "2.1.0-beta.2",
+        "webpack-merge": "^1.1.2"
+    }
+}```
 
 package.express.json
 
 ```json
 {
-    "body-parser": "^1.15.2",
-    "cookie-parser": "^1.4.3",
-    "express": "^4.14.0"
+    "dependencies":
+    {
+        "body-parser": "^1.15.2",
+        "cookie-parser": "^1.4.3",
+        "express": "^4.14.0"
+    }
 }
 ```
 
@@ -29,6 +46,11 @@ Drop your package files into project (eg: config/package.dev.json, config/packag
 Run
 ```
 npm-generate-dep config/package.dev.json config/package.express.json
+
+or just:
+
+npm-generate-dep config/package.*
 ```
 
-It will fetch all dependencies from those files and add them to package.json dependencies section by running "npm install"
+It will merge all dependencies from those files and add them to package.json dependencies section by running "npm install"
+
--- bin/npm-generate-dep.js
+++ bin/npm-generate-dep.js
@@ -2,35 +2,68 @@
 'use strict';
 
 var args = process.argv.slice(2);
-
 var npm = require('npm');
-npm.load({ 'save-dev': true }, function(err) {
-    if (err) {
-        console.error(err);
-        return;
-    }
 
-    var packages = [];
-    for (var i = 0; i < args.length; i++) {
-        var fs = require('fs');
-        var obj = JSON.parse(fs.readFileSync(args[i], 'utf8'));
-        for (var packageName in obj) {
-            // console.log(packageName+'@'+obj[packageName]);
-            packages.push(packageName+'@'+obj[packageName]);
-        }
+var managed = {};
+var packages = [];
+for (var i = 0; i < args.length; i++) {
+    var fs = require('fs');
+    var obj = JSON.parse(fs.readFileSync(args[i], 'utf8'));
+    if (obj.dependencies)
+    for (var packageName in obj.dependencies) {
+        packages.push(packageName+'@'+obj.dependencies[packageName]);
+        managed[packageName] = true;
     }
+}
+var packagesDev = [];
+for (var i = 0; i < args.length; i++) {
+    var fs = require('fs');
+    var obj = JSON.parse(fs.readFileSync(args[i], 'utf8'));
+    if (obj.devDependencies)
+    for (var packageName in obj.devDependencies) {
+        packagesDev.push(packageName+'@'+obj.devDependencies[packageName]);
+        managed[packageName] = true;
+    }
+}
 
-    npm.commands.install(packages, function(err, data) {
+
+function install(params, packages, cb) {
+    npm.load(params, function(err) {
         if (err) {
             console.error(err);
             return;
         }
-        console.log(data);
-        // log errors or data
-    });
 
-    npm.on('log', function(message) {
-        // log installation progress
-        console.log(message);
+        npm.commands.install(packages, function(err, data) {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            cb(data);
+        });
+
+        npm.on('log', function(message) {
+            console.log(message);
+        });
     });
+}
+
+install({ 'save-dev': true }, packagesDev, function () {
+    install({ 'save': true }, packages, function () {
+        var json = JSON.parse(fs.readFileSync('package.json', 'utf8'))
+        var devDependencies = json.devDependencies || {};
+        var dependencies = json.dependencies || {};
+
+        var unmanaged = {};
+        for (var k in dependencies) {
+            if (!managed[k]) unmanaged[k] = dependencies[k];
+        }
+        var devUnmanaged = {};
+        for (var k in devDependencies) {
+            if (!managed[k]) devUnmanaged[k] = devDependencies[k];
+        }
+
+        console.warn('unmanaged dependencies:', unmanaged);
+        console.warn('unmanaged devDependencies:', devUnmanaged);
+    })
 });
